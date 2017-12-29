c7b1228100469f4e4fe66f249d3974f2d07403d7
Bren Briggs <briggs.brenton@gmail.com>
2017-12-29 13:02:08 -0600

    And the award for the world's worst testing script goes to...

---
+++ test.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -e
+
+docker build .
+
+# Build all the versions sequentially. Pack a lunch, this takes a while.
+cd ./1.8 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
+cd ./1.8.3 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
+cd ./1.8.7 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
+cd ./1.8.8 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
+cd ./1.9 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
+cd ./1.9.2 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
+cd ./1.9.4 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
+cd ./1.10 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
+cd ./1.11 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
+cd ./1.12 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
+cd ./1.12.1 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
