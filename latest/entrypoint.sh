1a159eba37f95b875817265e85ab513e9d594b6a
Bren Briggs <briggs.brenton@gmail.com>
2018-03-05 21:45:58 -0500

    Fix broken tags from upstream Bukkit repos

---
+++ latest/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Solution borrowed from https://github.com/itzg/dockerfiles/blob/master/minecraft-server/start-minecraft.sh
+if [ ! -f /data/eula.txt ]; then
+  if [ "$EULA" != "" ]; then
+    echo "# Generated via Docker on $(date)" > eula.txt
+    echo "eula=$EULA" > /data/eula.txt
+  else
+    echo ""
+    echo "Please accept the Minecraft EULA at"
+    echo "  https://account.mojang.com/documents/minecraft_eula"
+    echo "by adding the following immediately after 'docker run':"
+    echo "  -e EULA=TRUE"
+    echo "or editing eula.txt to 'eula=true' in your server's data directory."
+    echo ""
+    exit 1
+  fi
+fi
+cd /data
+if [[ "$TRAVIS" = true ]]; then
+    echo "stop" | java -jar /root/$1-1.12.2.jar
+else
+    java -jar /root/$1-1.12.2.jar
+fi
