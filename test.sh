6b405ce501d7e45310f9b98394a15f799743d6d6
Bren Briggs <briggs.brenton@gmail.com>
2018-03-05 20:16:53 -0500

    FIX THE TYPO AGAIN

--- test.sh
+++ test.sh
@@ -2,17 +2,4 @@
 
 set -e
 
-cd $TAG && docker build -t bukkit && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit
-# Build all the versions sequentially. Pack a lunch, this takes a while.
-#cd ./1.8 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
-#cd ./1.8.3 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
-#cd ./1.8.7 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
-#cd ./1.8.8 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
-#cd ./1.9 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
-#cd ./1.9.2 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
-#cd ./1.9.4 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
-#cd ./1.10 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
-#cd ./1.11 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
-#cd ./1.12 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
-#cd ./1.12.1 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
-#cd ./1.12.2 && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit && cd ..
+cd $TAG && docker build -t bukkit . && docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bukkit
