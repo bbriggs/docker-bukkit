004fab3c17bd78255afe0e1ba235d36e0ff01684
Bren Briggs <bbriggs@users.noreply.github.com>
2020-01-04 18:44:17 -0500

    Update travis-build to handle "latest" tag (#28)
    * Update travis-build to handle "latest" tag
    
    * Use the 1.14 dockerfile to build "latest"
    
    Signed-off-by: Bren Briggs <code@fraq.io>
    
    Co-authored-by: Nathan Gaida <nfgaida@gmail.com>

--- travis-build.sh
+++ travis-build.sh
@@ -2,18 +2,32 @@
 
 set -xe
 
-# Build
-if [[ -z "$DOCKERFILE" ]]; then
-	docker build --build-arg BUKKIT_VERSION=$TAG -t bbriggs/bukkit:$TAG .
-else
-	docker build -f $DOCKERFILE --build-arg BUKKIT_VERSION=$TAG -t bbriggs/bukkit:$TAG .
-fi
+build_latest () {
+	docker build -f Dockerfile-1.14 -t bbriggs/bukkit:latest .
+}
 
-# Test
-docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bbriggs/bukkit:$TAG
+build_tag () {
+	# Build
+	if [[ -z "$DOCKERFILE" ]]; then
+		docker build --build-arg BUKKIT_VERSION=$TAG -t bbriggs/bukkit:$TAG .
+	else
+		docker build -f $DOCKERFILE --build-arg BUKKIT_VERSION=$TAG -t bbriggs/bukkit:$TAG .
+	fi
+}
 
-if [[ "$TRAVIS_BRANCH" = "master" ]]; then
-	docker push bbriggs/bukkit:$TAG
+test () {
+	# Test
+	docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bbriggs/bukkit:$TAG
+
+	if [[ "$TRAVIS_BRANCH" = "master" ]]; then
+		docker push bbriggs/bukkit:$TAG
+	else
+		exit 0
+	fi
+}
+
+if [ $TAG = "latest" ]; then
+	build_latest
 else
-	exit 0
+	build_tag
 fi
